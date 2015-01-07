034341525cd609a8e370e8f08d81ceb617ca2dcc
dhaak@mi.rwth-aachen.de <dhaak@mi.rwth-aachen.de@a6cd8b35-e117-d975-f05d-c74b452c09e8>
2015-01-07 08:37:51 +0000

    Version number increased

--- bin/footer.php
+++ bin/footer.php
@@ -7,8 +7,8 @@ echo '	<!-- Footer -->
 					<td width="120">
 					</td>
 					<td width="784">
-						OC-Big, Version 1.0.0, <a target="_blank" href="http://opensource.org/licenses/BSD-2-Clause">Licensed under the BSD-2-Clauses license</a>
-						<br/> Copyright 2013, Daniel Haak, Johan Gehlen, Praveen Sripad, Thomas M. Deserno. Department of Medical Informatics. RWTH Aachen. 
+						OC-Big, Version 1.1.0, <a target="_blank" href="http://opensource.org/licenses/BSD-2-Clause">Licensed under the BSD-2-Clauses license</a>
+						<br/> Copyright 2015, Daniel Haak, Johan Gehlen, Praveen Sripad, Thomas M. Deserno. Department of Medical Informatics. RWTH Aachen. 
 					</td>
 					<td width="120">
 					</td>
