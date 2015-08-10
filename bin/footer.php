43ded13cea92f0c638c81f833a2993761b1bdb17
idmteam <dhaak@mi.rwth-aachen.de>
2015-08-10 15:27:00 +0200

    version 1.1.2 introduced

--- bin/footer.php
+++ bin/footer.php
@@ -7,7 +7,7 @@ echo '	<!-- Footer -->
 					<td width="120">
 					</td>
 					<td width="784">
-						OC-Big, Version 1.1.1, <a target="_blank" href="http://opensource.org/licenses/BSD-2-Clause">Licensed under the BSD-2-Clauses license</a>
+						OC-Big, Version 1.1.2, <a target="_blank" href="http://opensource.org/licenses/BSD-2-Clause">Licensed under the BSD-2-Clauses license</a>
 						<br/> Copyright 2015, Daniel Haak, Johan Gehlen, Praveen Sripad, Thomas M. Deserno. Department of Medical Informatics. RWTH Aachen. 
 					</td>
 					<td width="120">
